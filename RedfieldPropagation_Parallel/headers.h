8efaff6d961ceda42d5f35534f3cd0ef146ebf7e
flo <fhase@g.harvard.edu>
2017-04-19 16:48:43 -0400

    Fixing merge conflicts

--- RedfieldPropagation_Parallel/headers.h
+++ RedfieldPropagation_Parallel/headers.h
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 #ifndef FUNC_LIOUVILLE_INCLUDED
 	#define FUNC_LIOUVILLE_INCLUDED
 
@@ -10,16 +9,6 @@
 	void hamiltonian_commutator(double *rho_real, double *rho_imag, double *hamiltonian, double *comm_real, double *comm_imag, int N);
 	#pragma acc routine gang
 	void lindblad_operator(double * restrict rho_real, double *rho_imag, double *gammas, double *eigVects, double *lindblad_real, double *lindblad_imag, double *links_to_loss, double *links_to_target, double *all_Vs, int SIZE);
-=======
-<<<<<<< HEAD
-#ifndef FUNC_LIOUVILLE_INCLUDED
-	#define FUNC_LIOUVILLE_INCLUDED
-
-	#pragma acc routine gang
-	void hamiltonian_commutator(double *rho_real, double *rho_imag, double *hamiltonian, double *comm_real, double *comm_imag, int N);
-	#pragma acc routine gang
-	void lindblad_operator(double *rho_real, double *rho_imag, double *gammas, double *eigVects, double *lindblad_real, double *lindblad_imag, double *links_to_loss, double *links_to_target, int SIZE);
->>>>>>> 4dd0983a47a7cf0708d39e1c642cc42d3cd430fa
 #endif
 
 /******************************************************************************************/
@@ -28,22 +17,16 @@
 	#define FUNC_RATES_INCLUDED
 	
 	void get_rates(double *gammas, double *params, double *energies, int num_params, int Nsites2);
-<<<<<<< HEAD
 //	#pragma acc routine worker
 	void get_V(double *V, double *eigvects, int i, int k, int N);
 	#pragma acc routine gang
 	void get_V_matrices(double *V, double *eigvects, int N);
-=======
-	#pragma acc routine worker
-	void get_V(double *V, double *eigvects, int i, int k, int N);
->>>>>>> 4dd0983a47a7cf0708d39e1c642cc42d3cd430fa
 #endif
 
 /******************************************************************************************/
 #ifndef FUNC_MMO_INCLUDED
 	#define FUNC_MMO_INCLUDED
 
-<<<<<<< HEAD
 //	#pragma acc routine worker
 	void matrix_add_real(double *A, double *B, double *C, int N);
 
@@ -57,19 +40,6 @@
 	void matrix_mul_scalar(double *A, double scalar, int N);
 
 	void matrix_add_complex(double *A_real, double *A_imag, double *B_real, double *B_imag, double *C_real, double *C_imag, int N);
-=======
-	#pragma acc routine worker
-	void matrix_add_real(double *A, double *B, double *C, int N);
-
-	#pragma acc routine worker
-	void matrix_sub_real(double *A, double *B, double *C, int N);
-
-	#pragma acc routine worker
-	void matrix_mul_real(double *A, double *B, double *C, int N);
-
-	#pragma acc routine worker
-	void matrix_mul_scalar(double *A, double scalar, int N);
->>>>>>> 4dd0983a47a7cf0708d39e1c642cc42d3cd430fa
 #endif
 
 /******************************************************************************************/
@@ -79,11 +49,7 @@
 	#define FUNC_MAT_GEN_INCLUDED
 
 	void gen_identity_matrix(double *A, int N);
-<<<<<<< HEAD
 //	#pragma acc routine worker
-=======
-	#pragma acc routine worker
->>>>>>> 4dd0983a47a7cf0708d39e1c642cc42d3cd430fa
 	void gen_zero_matrix_real(double *A, int N);
 	void gen_one_matrix_real(double *A, int N);
 	void gen_random_matrix_real(double *mat, int N);
@@ -92,11 +58,7 @@
 	void gen_test_links(double *links_to_loss, double *links_to_target, int N);
 	void gen_test_spec_densities(double *params, int N);
 	void gen_identity_complex(double *A_real, double *A_imag, int N);
-<<<<<<< HEAD
 //	#pragma acc routine worker
-=======
-	#pragma acc routine worker
->>>>>>> 4dd0983a47a7cf0708d39e1c642cc42d3cd430fa
 	void gen_zero_matrix_complex(double *A_real, double *A_imag, int N);
 
 
@@ -107,7 +69,6 @@
 	#define FUNC_SMO_INCLUDED
 	// from single_matrix_operations.c
 	void diagonalize(double *A, double *D, int N);
-<<<<<<< HEAD
 //	#pragma acc routine worker
 	void rotate(double *A, double *eigvect, int N);
 //	#pragma acc routine worker
@@ -123,55 +84,3 @@
 #endif
 
 
-=======
-	#pragma acc routine worker
-	void rotate(double *A, double *eigvect, int N);
-	#pragma acc routine worker
-	void transpose(double *A, int N);
-#endif
-=======
-// from liouville_redfield.c
-
-void hamiltonian_commutator(double *rho_real, double *rho_imag, double *hamiltonian, double *comm_real, double *comm_imag, int N);
-
-// from matrix_generators.c
-void gen_identity_matrix(double *A, int N);
-#pragma acc routine
-void gen_zero_matrix_real(double *A, int N);
-void gen_one_matrix_real(double *A, int N);
-void gen_random_matrix_real(double *mat, int N);
-void gen_random_hamiltonian_real(double *H, int N);
-void gen_test_hamiltonian(double *A);
-void gen_test_links(double *links_to_loss, double *links_to_target, int N);
-void gen_test_spec_densities(double *params, int N);
-void gen_identity_complex(double *A_real, double *A_imag, int N);
-void gen_zero_matrix_complex(double *A_real, double *A_imag, int N);
-
-
-// from multiple_matrix_operations.c
-#pragma acc routine
-void matrix_add_real(double *A, double *B, double *C, int N);
-#pragma acc routine 
-void matrix_sub_real(double *A, double *B, double *C, int N);
-#pragma acc routine
-void matrix_mul_real(double *A, double *B, double *C, int N);
-#pragma acc routine
-void matrix_mul_scalar(double *A, double scalar, int N);
-
-// from rates.c
-void get_rates(double *gammas, double *params, double *energies, int num_params, int Nsites2);
-#pragma acc routine
-void get_V(double *V, double *eigvects, int i, int k, int N);
-
-// from single_matrix_operations.c
-void diagonalize(double *A, double *D, int N);
-#pragma acc routine
-void rotate(double *A, double *eigvect, int N);
-#pragma acc routine
-void transpose(double *A, int N);
-
-
-// from utilities.c
-void print_matrix_real(double *A, int N);
->>>>>>> 8a8db8f89679f383fb17ba16f8a581ae653ec48e
->>>>>>> 4dd0983a47a7cf0708d39e1c642cc42d3cd430fa
--- RedfieldPropagation_Parallel/liouville_redfield.c
+++ RedfieldPropagation_Parallel/liouville_redfield.c
@@ -3,14 +3,7 @@
 #include <stdlib.h>
 
 #include "headers.h"
-<<<<<<< HEAD
 //#include "multiple_matrix_operations.h"
-=======
-<<<<<<< HEAD
-//#include "multiple_matrix_operations.h"
-=======
->>>>>>> 8a8db8f89679f383fb17ba16f8a581ae653ec48e
->>>>>>> 4dd0983a47a7cf0708d39e1c642cc42d3cd430fa
 
 /* methods for computing the Liouville operator on the density matrix
  * ... all methods assume operators to be in the exciton basis, unless otherwise specified
@@ -28,86 +21,14 @@
 #define T    300.0 // FIXME: temperature should be a parameter
 #define BETA 1. / (8.6173303e-5*eV_to_cm1 * 300.0)
 
-<<<<<<< HEAD
-
 /********************************************************************/
 
-#pragma acc routine gang
-void hamiltonian_commutator(double *rho_real, double *rho_imag, double *hamiltonian, double *comm_real, double *comm_imag, int N) {
-=======
-/********************************************************************/
-
-<<<<<<< HEAD
 #pragma acc routine gang
 void hamiltonian_commutator(double *rho_real, double *rho_imag, double *hamiltonian, double *comm_real, double *comm_imag, int N) {
 	// as dense as possible implementation of the hamiltonian commutator -i * [H, rho] / hbar
 	int unsigned i, j;
 	#pragma acc data present_or_copyin(hamiltonian[0:N]) copyin(rho_real[0:N*N], rho_imag[0:N*N]) present_or_copy(comm_real[0:N*N], comm_imag[0:N*N])
 	#pragma acc loop independent
-=======
- void hamiltonian_commutator(double *rho_real, double *rho_imag, double *hamiltonian, double *comm_real, double *comm_imag, int N) {
->>>>>>> 8a8db8f89679f383fb17ba16f8a581ae653ec48e
- 	// FIXME
- 	// the hamiltonian is a real valued diagonal matrix
- 	// ... but we don't care for now and use simple matrix multiplication
-
-<<<<<<< HEAD
-	int unsigned i, j;
-	// hamiltonian is a simple one dimensional array
-	
-	double *help1_real, *help1_imag, *help2_real, *help2_imag;
-	help1_real = (double *) malloc(sizeof(double) * (N * N));
-=======
- 	double *h_real, *h_imag; 
- 	h_real = (double *) malloc(sizeof(double) * (N * N));
- 	h_imag = (double *) malloc(sizeof(double) * (N * N));
- 	gen_zero_matrix_real(h_real, N);
- 	gen_zero_matrix_real(h_imag, N);
-
- 	double *help1_real, *help1_imag, *help2_real, *help2_imag;
- 	help1_real = (double *) malloc(sizeof(double) * (N * N));
->>>>>>> 8a8db8f89679f383fb17ba16f8a581ae653ec48e
-	help2_real = (double *) malloc(sizeof(double) * (N * N));
- 	help1_imag = (double *) malloc(sizeof(double) * (N * N));
-	help2_imag = (double *) malloc(sizeof(double) * (N * N));
-
-
-	// get the first part of the commutator
-<<<<<<< HEAD
-	#pragma acc data present(rho_real[0:N*N], rho_imag[0:N*N], hamiltonian[0:N]) create(help1_real[0:N*N], help1_imag[0:N*N]) 
-	#pragma acc loop gang
-	for (i = 0; i < N; i++) {
-		#pragma acc loop gang, worker
-		for (j = 0; j < N; j++) {
-			help1_real[i + j * N] = hamiltonian[j] * rho_real[i + j * N];
-			help1_imag[i + j * N] = 0;
-		}
-	}
-
-	// get the second part of the commutator rho * H
-	#pragma acc data present(rho_real[0:N*N], rho_imag[0:N*N], hamiltonian[0:N]) create(help2_real[0:N*N], help2_imag[0:N*N])
-	#pragma acc loop gang
-	for (i = 0; i < N; i++) {
-		#pragma acc loop gang, worker
-		for (j = 0; j < N; j++) {
-			help2_real[i + j * N] = hamiltonian[i] * rho_real[i + j * N];
-			help2_imag[i + j * N] = 0;
-		}
-	}
-	
-	
-	// combine the two parts
-	#pragma acc data present(help1_real[0:N*N], help1_imag[0:N*N], help2_real[0:N*N], help2_imag[0:N*N], comm_real[0:N*N], comm_imag[0:N*N])
-	matrix_sub_real(help1_imag, help2_imag, comm_real, N);
-	matrix_sub_real(help2_real, help1_real, comm_imag, N);
-
-	matrix_mul_scalar(comm_real, HBAR_INV, N);
-	matrix_mul_scalar(comm_imag, HBAR_INV, N);
-	#pragma end data
-
-=======
-	int unsigned i;
->>>>>>> 4dd0983a47a7cf0708d39e1c642cc42d3cd430fa
 	for (i = 0; i < N; i++) {
 		#pragma acc loop independent
 		for (j = 0; j < N; j++) {
@@ -118,40 +39,13 @@ void hamiltonian_commutator(double *rho_real, double *rho_imag, double *hamilton
 }
 
 
-<<<<<<< HEAD
-=======
-	free((void*) h_real);
-	free((void*) h_imag);
->>>>>>> 8a8db8f89679f383fb17ba16f8a581ae653ec48e
-	free((void*) help1_real);
-	free((void*) help1_imag);
-	free((void*) help2_real);
-	free((void*) help2_imag);
-	
-<<<<<<< HEAD
-=======
-	#pragma end data
->>>>>>> 8a8db8f89679f383fb17ba16f8a581ae653ec48e
-	
- }
->>>>>>> 4dd0983a47a7cf0708d39e1c642cc42d3cd430fa
 
 
 
 #pragma acc routine gang
-<<<<<<< HEAD
 void lindblad_operator(double * restrict rho_real, double *rho_imag, double *gammas, double *eigVects, double *lindblad_real, double *lindblad_imag, double *links_to_loss, double *links_to_target, double *all_Vs, int SIZE) {
 	int unsigned i, j, k, m, M, N;
 	double rate, sum;	
-=======
-void lindblad_operator(double *rho_real, double *rho_imag, double *gammas, double *eigVects, double *lindblad_real, double *lindblad_imag, double *links_to_loss, double *links_to_target, int SIZE) {
-	int unsigned m, M, N;
-<<<<<<< HEAD
-	double restrict rate;
-=======
-	double rate;
->>>>>>> 8a8db8f89679f383fb17ba16f8a581ae653ec48e
->>>>>>> 4dd0983a47a7cf0708d39e1c642cc42d3cd430fa
 
 	double * restrict V;
 	V = (double *) malloc(sizeof(double) * SIZE * SIZE);
@@ -163,7 +57,6 @@ void lindblad_operator(double *rho_real, double *rho_imag, double *gammas, doubl
 	helper      = (double *) malloc(sizeof(double) * SIZE * SIZE);
 
 
-<<<<<<< HEAD
 	{
 //	#pragma acc loop independent gang
 
@@ -276,88 +169,15 @@ void lindblad_operator(double *rho_real, double *rho_imag, double *gammas, doubl
 						lindblad_real[i + j * SIZE] += rate * (first_real[i + j * SIZE] - second_real[i + j * SIZE]);
 					}
 				}
-=======
-//       #pragma acc data copyin(rho_real[0:N*N], rho_imag[0:N*N], h_real[0:N*N], h_imag[0:N*N]) create(help1_real[0:N*N], help1_imag[0:N*N], help2_real[0:N*N], help2_imag[0:N*N]) copy(comm_real[0:N*N], comm_imag[0:N*N])
- 
-	// decay of excitons into other excitonic states
-//	#pragma acc data copyin(gammas[0:SIZE*SIZE*SIZE])
-//	#pragma acc parallel loop tile(16, 16) gang vector
-<<<<<<< HEAD
-//	#pragma acc data copyin(V[0:SIZE*SIZE], V_dagg[0:SIZE*SIZE], gammas[0:SIZE*SIZE*SIZE], helper[0:SIZE*SIZE], first_real[0:SIZE*SIZE], second_real[0:SIZE*SIZE], third_real[0:SIZE*SIZE]) copy(rho_real[0:SIZE*SIZE], rho_imag[0:SIZE*SIZE])
-	#pragma acc declare copyin(SIZE) 
-	#pragma acc data create(helper[0:SIZE*SIZE], first_real[0:SIZE*SIZE], second_real[0:SIZE*SIZE], third_real[0:SIZE*SIZE]) present_or_copyin(V[0:SIZE*SIZE], V_dagg[0:SIZE*SIZE]) present(rho_real[0:SIZE*SIZE], rho_imag[0:SIZE*SIZE], gammas[0:SIZE*SIZE*SIZE], lindblad_real[0:SIZE*SIZE], lindblad_imag[0:SIZE*SIZE])
-//	#pragma acc loop gang
-=======
-	#pragma acc data copyin(V[0:SIZE*SIZE], V_dagg[0:SIZE*SIZE], gammas[0:SIZE*SIZE*SIZE], helper[0:SIZE*SIZE], first_real[0:SIZE*SIZE], second_real[0:SIZE*SIZE], third_real[0:SIZE*SIZE]) copy(rho_real[0:SIZE*SIZE], rho_imag[0:SIZE*SIZE])
- 
-
->>>>>>> 8a8db8f89679f383fb17ba16f8a581ae653ec48e
-	for (m = 1; m < SIZE - 1; m++) {
-		get_V(V, eigVects, m, m, SIZE);
-		get_V(V_dagg, eigVects, m, m, SIZE);
-		transpose(V_dagg, SIZE);
-<<<<<<< HEAD
-
-//		#pragma acc loop gang, worker
-		for (M = 0; M < SIZE; M++) {
-//			#pragma acc loop gang, worker
-=======
-		#pragma acc loop independent
-		for (M = 0; M < SIZE; M++) {
-			#pragma acc loop independent
->>>>>>> 8a8db8f89679f383fb17ba16f8a581ae653ec48e
-			for (N = 0; N < SIZE; N++) {
-
-				rate = gammas[M + N * SIZE + m * SIZE * SIZE];
-				rate = (double)(rate);
-<<<<<<< HEAD
-		
-=======
-
->>>>>>> 8a8db8f89679f383fb17ba16f8a581ae653ec48e
-//				get_V(V, eigVects, m, m, SIZE);
-//				get_V(V_dagg, eigVects, m, m, SIZE);
-//				transpose(V_dagg, SIZE);
-
-				matrix_mul_real(V, rho_real, helper, SIZE);
-				matrix_mul_real(helper, V_dagg, first_real, SIZE);
-
-				matrix_mul_real(V, rho_real, helper, SIZE);
-				matrix_mul_real(V_dagg, helper, second_real, SIZE);
-				matrix_mul_scalar(second_real, 0.5, SIZE);
-
-				matrix_mul_real(V_dagg, V, helper, SIZE);
-				matrix_mul_real(rho_real, helper, third_real, SIZE);
-				matrix_mul_scalar(third_real, 0.5, SIZE);	
-
-				matrix_sub_real(first_real, second_real, first_real, SIZE);
-				matrix_sub_real(first_real, third_real, first_real, SIZE);
-				matrix_mul_scalar(first_real, rate, SIZE);
-
->>>>>>> 4dd0983a47a7cf0708d39e1c642cc42d3cd430fa
 
 
 			}
 		}
 	}
 
-<<<<<<< HEAD
 
 	#pragma acc data present_or_copyin(all_Vs[0:SIZE*SIZE*SIZE*3], V[0:SIZE*SIZE], helper[0:SIZE*SIZE]) copyin(rho_real[0:SIZE*SIZE], rho_imag[0:SIZE*SIZE])
 	#pragma acc loop gang
-=======
-<<<<<<< HEAD
-//	#pragma end data
-
-	// decay of excitons into the ground (loss) state
-	#pragma acc data create(V[0:SIZE*SIZE], V_dagg[0:SIZE*SIZE], helper[0:SIZE*SIZE], first_real[0:SIZE*SIZE], second_real[0:SIZE*SIZE], third_real[0:SIZE*SIZE]) present(rho_real[0:SIZE*SIZE], rho_imag[0:SIZE*SIZE], gammas[0:SIZE*SIZE*SIZE], lindblad_real[0:SIZE*SIZE], lindblad_imag[0:SIZE*SIZE])
-	#pragma acc loop gang
-=======
-	#pragma end data
-
-	// decay of excitons into the ground (loss) state
->>>>>>> 8a8db8f89679f383fb17ba16f8a581ae653ec48e
->>>>>>> 4dd0983a47a7cf0708d39e1c642cc42d3cd430fa
 	for (m = 0; m < SIZE; m++) {
 		rate = links_to_loss[m];
 
@@ -459,24 +279,11 @@ void lindblad_operator(double *rho_real, double *rho_imag, double *gammas, doubl
 		}		
 	}
 
-<<<<<<< HEAD
 
 	// decay of excitons into the target (reaction center) state
         
 	#pragma acc data present_or_copyin(all_Vs[0:SIZE*SIZE*SIZE*3], V[0:SIZE*SIZE], helper[0:SIZE*SIZE]) copyin(rho_real[0:SIZE*SIZE], rho_imag[0:SIZE*SIZE])
 	#pragma acc loop gang
-=======
-<<<<<<< HEAD
-///	#pragma end data
-
-	// decay of excitons into the target (reaction center) state
-//	#pragma acc data create(V[0:SIZE*SIZE], V_dagg[0:SIZE*SIZE], helper[0:SIZE*SIZE], first_real[0:SIZE*SIZE], second_real[0:SIZE*SIZE], third_real[0:SIZE*SIZE]) present(rho_real[0:SIZE*SIZE], rho_imag[0:SIZE*SIZE], gammas[0:SIZE*SIZE*SIZE], lindblad_real[0:SIZE*SIZE], lindblad_imag[0:SIZE*SIZE])
-	#pragma acc data create(V[0:SIZE*SIZE], V_dagg[0:SIZE*SIZE], helper[0:SIZE*SIZE], first_real[0:SIZE*SIZE], second_real[0:SIZE*SIZE], third_real[0:SIZE*SIZE]) present(rho_real[0:SIZE*SIZE], rho_imag[0:SIZE*SIZE], gammas[0:SIZE*SIZE*SIZE], lindblad_real[0:SIZE*SIZE], lindblad_imag[0:SIZE*SIZE])
-	#pragma acc loop gang
-=======
-	// decay of excitons into the target (reaction center) state
->>>>>>> 8a8db8f89679f383fb17ba16f8a581ae653ec48e
->>>>>>> 4dd0983a47a7cf0708d39e1c642cc42d3cd430fa
 	for (m = 0; m < SIZE; m++) {
 		rate = links_to_target[m];
 
@@ -573,7 +380,6 @@ void lindblad_operator(double *rho_real, double *rho_imag, double *gammas, doubl
 		}
 	//--------
 
-<<<<<<< HEAD
 		// subtract first_real - second_real and multiply by the rate
 		#pragma acc loop independent
 		for (j = 0; j < SIZE; j++) {
@@ -584,12 +390,6 @@ void lindblad_operator(double *rho_real, double *rho_imag, double *gammas, doubl
 		}
 	} // end of acc data directive
 	} 
-=======
-<<<<<<< HEAD
-	#pragma end data 
-=======
->>>>>>> 8a8db8f89679f383fb17ba16f8a581ae653ec48e
->>>>>>> 4dd0983a47a7cf0708d39e1c642cc42d3cd430fa
 
 	free((void*) V);
 	free((void*) first_real);
@@ -601,20 +401,12 @@ void lindblad_operator(double *rho_real, double *rho_imag, double *gammas, doubl
 
 
 
-<<<<<<< HEAD
 #pragma acc routine gang
-=======
-<<<<<<< HEAD
-#pragma acc routine gang
-=======
->>>>>>> 8a8db8f89679f383fb17ba16f8a581ae653ec48e
->>>>>>> 4dd0983a47a7cf0708d39e1c642cc42d3cd430fa
 void get_density_update(double *rho_real, double *rho_imag, double *energies, double *comm_real, double *comm_imag, 
 	                    double *gammas, double *eigvects, double *lindblad_real, double *lindblad_imag, double *links_to_loss, double *links_to_target, double *all_Vs, int N) {
 
 	hamiltonian_commutator(rho_real, rho_imag, energies, comm_real, comm_imag, N);
 
-<<<<<<< HEAD
 	int unsigned i, j;
 	#pragma acc loop independent
 	for (i = 0; i < N; i++) {
@@ -628,10 +420,3 @@ void get_density_update(double *rho_real, double *rho_imag, double *energies, do
 	lindblad_operator(rho_real, rho_imag, gammas, eigvects, lindblad_real, lindblad_imag, links_to_loss, links_to_target, all_Vs, N);
 
 }
-=======
-<<<<<<< HEAD
-}
-=======
-}
->>>>>>> 8a8db8f89679f383fb17ba16f8a581ae653ec48e
->>>>>>> 4dd0983a47a7cf0708d39e1c642cc42d3cd430fa
