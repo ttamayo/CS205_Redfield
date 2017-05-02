79989c050a1937ca7e38c334e7d842328cc529a5
flo <fhase@g.harvard.edu>
2017-05-02 12:43:47 -0400

    Cleaned up the directory

--- .RedfieldPropagation_Serial/multiple_matrix_operations.h
+++
@@ -1,8 +0,0 @@
-
-#ifdef FUNC_MMO_INCLUDED
-	#define FUNC_MMO_INCLUDED
-	void matrix_add_real(double *A, double *B, double *C, int N);
-	void matrix_sub_real(double *A, double *B, double *C, int N);
-	void matrix_mul_real(double *A, double *B, double *C, int N);
-	void matrix_mul_scalar(double *A, double scalar, int N);
-#endif
--- CDraft/test/README.md
+++
@@ -1,2 +0,0 @@
-This folder contains test code of Lapack and Blas function
-of matrix multiplication and diagonalization of complex matrices.
